d378b099ea2fb53ed8dbaf68f7d30be1156acfdb
DanielCarrami <danielcarrami@gmail.com>
2018-10-30 17:27:02 -0600

    Branch de registro

--- app/src/main/java/com/example/carlo/androidapp/LoginActivity.java
+++ app/src/main/java/com/example/carlo/androidapp/LoginActivity.java
@@ -2,15 +2,14 @@ package com.example.carlo.androidapp;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -19,11 +18,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.auth.api.signin.SignInAccount;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
---
+++ app/src/main/java/com/example/carlo/androidapp/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.carlo.androidapp;
+
+public class RegisterActivity {
+}
