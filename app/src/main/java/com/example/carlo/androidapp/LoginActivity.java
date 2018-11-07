0ee86b00725c46b7ee35f7f95e304f14b6da8c65
Carlos Parrodi <carlosparrodimartinez@hotmail.com>
2018-11-06 20:33:38 -0600

    Ya se crean usuarios al hacer login con fb, falta limiar e implementarlo en el registro

--- app/src/main/java/com/example/carlo/androidapp/LoginActivity.java
+++ app/src/main/java/com/example/carlo/androidapp/LoginActivity.java
@@ -16,15 +16,22 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -44,6 +51,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     private GoogleApiClient googleApiClient;
     private static final int SIGN_IN_CODE = 777;
+    public String answer;
 
     TextView textview, registro;
     EditText em, psw;
@@ -61,6 +69,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         em = (EditText)findViewById(R.id.editTextEmail);
         psw = (EditText)findViewById(R.id.editTextPwd);
         registro = (TextView)findViewById(R.id.textViewANR);
+        textview = (TextView)findViewById(R.id.textViewOC);
+
         registro.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -74,21 +84,36 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    String email = object.getString("email");
+                                    String name = object.getString("name");
+                                    String password = object.getString("id");
+                                    new AddManager().execute("http://ertourister.appspot.com/user/add", name, email, password);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    textview.setText("no se pudieron obtener los datos de FB");
+                                }
 
-                        Toast.makeText(LoginActivity.this, loginResult.getAccessToken().getUserId(),
-                                Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email");
+                        request.setParameters(parameters);
+                        request.executeAsync();
                     }
 
                     @Override
                     public void onCancel() {
-                        Toast.makeText(LoginActivity.this, "FacebookCancel",
-                                Toast.LENGTH_SHORT).show();
+
+                        textview.setText("Facebook Cancel");
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
-                        Toast.makeText(LoginActivity.this, "Facebook Error",
-                                Toast.LENGTH_SHORT).show();
+                       textview.setText("Facebook Error");
                     }
                 });
 
@@ -104,6 +129,156 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
 
+    private class  BasicLoginManager extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuffer response = new StringBuffer();
+
+            try{
+                URL url = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                HashMap<String, String> postParams = new HashMap<>();
+                postParams.put("email", strings[1]);
+                postParams.put("password", strings[2]);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostString(postParams));
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK){
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while((line = br.readLine()) != null){
+                        response.append(line);
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
+
+
+        private String getPostString(HashMap<String, String> params)throws UnsupportedEncodingException {
+
+            StringBuffer sb = new StringBuffer();
+            boolean first = true;
+            for(Map.Entry<String, String> entry: params.entrySet()){
+                if(first)
+                    first = false;
+                else
+                    sb.append("&");
+                sb.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                sb.append("=");
+                sb.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+
+            }
+            return sb.toString();
+
+        }
+
+        protected void onPostExecute(String response){
+            try {
+                JSONObject jsonresponse = new JSONObject(response);
+                answer= jsonresponse.getString("info");
+            } catch (JSONException e) {
+                e.printStackTrace();
+                answer = "fail";
+            }
+
+            textview.setText(answer);
+        }
+
+    }
+
+    private class  AddManager extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuffer response = new StringBuffer();
+
+            try{
+                URL url = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                HashMap<String, String> postParams = new HashMap<>();
+                postParams.put("name", strings[1]);
+                postParams.put("email", strings[2]);
+                postParams.put("password", strings[3]);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostString(postParams));
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK){
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while((line = br.readLine()) != null){
+                        response.append(line);
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
+
+
+        private String getPostString(HashMap<String, String> params)throws UnsupportedEncodingException {
+
+            StringBuffer sb = new StringBuffer();
+            boolean first = true;
+            for(Map.Entry<String, String> entry: params.entrySet()){
+                if(first)
+                    first = false;
+                else
+                    sb.append("&");
+                sb.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                sb.append("=");
+                sb.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+
+            }
+            return sb.toString();
+
+        }
+
+        protected void onPostExecute(String response){
+            try {
+                JSONObject jsonresponse = new JSONObject(response);
+                answer= jsonresponse.getString("info");
+            } catch (JSONException e) {
+                e.printStackTrace();
+                answer = "fail to add";
+            }
+
+            textview.setText(answer);
+        }
+
+    }
+
+
     // Manages the results from FB and Google connections
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -113,6 +288,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }else callbackManager.onActivityResult(requestCode, resultCode, data);
+
+
 
     }
 
@@ -147,8 +324,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
     //Calls the method BasicLoginManager
     public void doBasicLogin(View v){
-        apiConnect connect = new apiConnect();
-        String response = connect.basicLogin("ertourister.appspot.com/user/login", em.getText().toString(), psw.getText().toString());
-        textview.setText(response);
+        new BasicLoginManager().execute("http://ertourister.appspot.com/user/login", em.getText().toString(), psw.getText().toString());
+
     }
 }
